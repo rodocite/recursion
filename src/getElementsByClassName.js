7fae5ce402014bf51c4f1b8366efae3cae80b84c
Rodolfo Yabut <rodolfoyabut@gmail.com>
2015-06-18 12:21:59 -0700

    Add comments to getElementsByCassName.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,12 +6,17 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
   var result = [];
+  //node variable used to initialize traversal
   var node = this.childNodes || document.childNodes;
 
+  //base case
   if(_.contains(this.classList, className)) {
     result.push(this);
   }
 
+  //recursive condition
+  //.call used to retain context for traversal
+  //also pushes the summation of valid values for the current position in the traversal
   _.each(node, function(childNode) {
     result.push(getElementsByClassName.call(childNode, className));
   });
